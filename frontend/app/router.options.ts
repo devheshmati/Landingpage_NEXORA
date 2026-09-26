0fd2960520c00bfa4db5653687062a289c19632b
devheshmati <reza2boyce@gmail.com>
2026-09-26 03:39:14 +0200

    remove some comment, close #3

--- frontend/app/router.options.ts
+++ frontend/app/router.options.ts
@@ -3,22 +3,18 @@ import type { RouterConfig } from '@nuxt/schema'
 
 export default <RouterConfig>{
   scrollBehavior(to, from, savedPosition) {
-    // اگر لینک دارای Hash باشد (مثلاً #features)
     if (to.hash) {
       return {
         el: to.hash,
         behavior: 'smooth',
-        // در صورت نیاز به offset برای هدر ثابت (Fixed Header)
         top: 80 
       }
     }
 
-    // بازگشت به موقعیت قبلی در صورت زدن دکمه Back مرورگر
     if (savedPosition) {
       return savedPosition
     }
 
-    // هدایت به بالای صفحه برای صفحات جدید
     return { top: 0, behavior: 'smooth' }
   }
 }
